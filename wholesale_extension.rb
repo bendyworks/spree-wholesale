f15e6483d0b8d06e984c65915894b16bf9d7fbd7
Matthew Lambie <mlambie@lambie.org>
2009-10-16 16:45:51 +0800

    Extended LineItem and ProductsHelper

--- wholesale_extension.rb
+++ wholesale_extension.rb
@@ -34,6 +34,27 @@ class WholesaleExtension < Spree::Extension
     Product.class_eval do
       delegate_belongs_to :master, :wholesale_price
     end
+    
+    # Reopen the ProductsHelper module and redefine the product_price method
+    ProductsHelper.class_eval do
+      def product_price(product_or_variant, options={})
+        options.assert_valid_keys(:format_as_currency, :show_vat_text)
+        options.reverse_merge! :format_as_currency => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
+        if ( !current_user.nil? && current_user.has_role?("wholesale") && !product_or_variant.wholesale_price.blank?)          
+          amount = product_or_variant.wholesale_price
+        else
+          amount = product_or_variant.price
+        end
+        amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
+        options.delete(:format_as_currency) ? format_price(amount, options) : ("%0.2f" % amount).to_f
+      end
+    end
+    
+    LineItem.class_eval do
+      def wholesale_price
+        price
+      end
+    end
 
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
