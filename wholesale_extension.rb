e2abd8425b63778fe28729755e6b3d3a63f31e4f
Matthew Lambie <mlambie@lambie.org>
2009-10-21 18:22:59 +0800

    A user's cart updates (wholesale) on login/logout

--- wholesale_extension.rb
+++ wholesale_extension.rb
@@ -99,6 +99,62 @@ class WholesaleExtension < Spree::Extension
         self.save!
       end
     end
+    
+    UserSessionsController.class_eval do
+      
+      def create
+        @user_session = UserSession.new(params[:user_session])
+        success = @user_session.save
+        respond_to do |format|
+          format.html {                                
+            if success 
+              logger.error("YO: #{session.inspect}")
+              modify_order_on_login
+              flash[:notice] = t("logged_in_succesfully")
+              redirect_back_or_default products_path
+            else
+              flash.now[:error] = t("login_failed")
+              render :new
+            end
+          }
+          format.js {
+            user = success ? @user_session.record : nil
+            render :json => user ? {:ship_address => user.ship_address, :bill_address => user.bill_address}.to_json : success.to_json
+          }
+        end    
+      end
+      
+      def destroy
+        # logger.error("YO: in session destroy")
+        modify_order_on_logout
+        current_user_session.destroy
+        flash[:notice] = t("logged_out")
+        redirect_to products_path
+      end
+      
+      private
+      def modify_order_on_login
+        user = User.find_by_id(session["user_credentials_id"]) || User.new
+        user.has_role?("wholesale") ? force_wholesale : force_retail
+      end
+      
+      def modify_order_on_logout
+        logger.error("YO: modify order on logout")
+        force_retail
+      end
+      
+      def force_retail
+        logger.error("YO: force retail")
+        order = Order.find_by_id(session[:order_id])
+        order.force_retail if !order.nil?
+      end
+      
+      def force_wholesale
+        logger.error("YO: force wholesale")
+        order = Order.find_by_id(session[:order_id])
+        order.force_wholesale if !order.nil?
+      end
+    end
 
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
